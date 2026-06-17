07cf2f2f45e219917a8affe6e30d087c80f6269c
nvrmndbruh <anton.kn59@gmail.com>
2026-06-17 02:55:23 +0000

    fix: адаптация проекта под новый сервер

--- backend/app/main.py
+++ backend/app/main.py
@@ -37,7 +37,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "http://185.50.202.253:5173",
+        "http://85.198.84.14",
     ],
     allow_credentials=True,
     allow_methods=["*"],
